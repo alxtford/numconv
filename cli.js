39d5e8443f83555659e2b9432d35918b1164518a
Alex Ford <alxtford@gmail.com>
2019-01-05 18:45:31 +0000

    Added custom usage, and adjusted help text

--- cli.js
+++ cli.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
+const figlet = require('figlet')
+
 const cli = require('sywac')
   .style(require('sywac-style-basic'))
+  .preface(figlet.textSync('numconv'))
   .outputSettings({ maxWidth: 100 })
-  .showHelpByDefault()
   .help('-h, --help')
   .version('-v, --version')
-
+  .positional('<in>', { paramsDesc: 'Input integer', mustExist: true})
+  .positional('<in type>', { paramsDesc: 'Type of input integer.', choices: ['hex', 'dec'], mustExist: true})
+  .positional('<out type>', { paramsDesc: 'Type of output integer.', choices: ['hex', 'dec'], mustExist: true})
 module.exports = cli
 
 if (require.main === module) cli.parseAndExit()
