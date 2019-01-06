b29905fbec9b86b9c0a0e5dc0497e2536f8210d1
Alex Ford <alxtford@gmail.com>
2019-01-06 15:28:17 +0000

    Basic hex to dec logic added

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 #!/usr/bin/env node
 
-// Tap added for testing
-// var tap = require('tap')
-
 // cli imported from cli.js
 var cli = require('./cli.js')
 
+var decHex = require('./dec-hex.js')
+var decToHex = decHex.decToHex
+var hexToDec = decHex.hexToDec
+
+var invalidOutputWarn = 'Invalid output type. The valid output types are: \'dec\', \'hex\''
+var invalidInputWarn = 'Invalid input type. The valid input types are: \'dec\', \'hex\''
+
 if (require.main === module) {
   cli.parseAndExit().then(argv => {
-    console.log(argv)
+    switch (argv.out_type) {
+      case 'hex':
+        console.log('Hex to be outputted!')
+
+        switch (argv.in_type) {
+          case 'dec':
+            console.log('Dec inputted!')
+            console.log('Converted num = ' + decToHex(argv.in))
+            break
+          default: {
+            console.log(invalidInputWarn)
+          }
+        }
+        break
+      case 'dec':
+        console.log('Dec to be outputted!')
+
+        switch (argv.in_type) {
+          case 'hex':
+            console.log('Hex inputted!')
+            console.log('Converted num = ' + hexToDec(argv.in))
+            break
+          default:
+            console.log(invalidInputWarn)
+        }
+        break
+      default: {
+        console.log(invalidOutputWarn)
+      }
+    }
   })
 }
