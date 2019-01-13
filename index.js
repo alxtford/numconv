1814ba114fa58b4e911d8e560fb0c278d5956648
Alex Ford <alxtford@gmail.com>
2019-01-13 01:10:27 +0000

    Console.log only happens in cli output. log is added to return obj

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const SUCCESS = 1
 // cli imported from cli.js
 var cli = require('./cli.js')
 
+// errCatch does all of the heavy checking
 var errCatch = require('./errCatch.js')
 
 function numConv (inVar, inBase, outBase) {
@@ -13,38 +14,48 @@ function numConv (inVar, inBase, outBase) {
 
   var returnObj = {
     var: 0,
-    err_code: SUCCESS
+    err_code: SUCCESS,
+    log: ''
   }
-
+  // Check each base, and return if an error is detected
   inBaseInternal = errCatch.baseErrCatch(inBase, true)
   outBaseInternal = errCatch.baseErrCatch(outBase, false)
   if (!inBaseInternal || !outBaseInternal) {
-    console.log('Invalid base detected. Please ensure you are using bases 2 to 32!')
+    returnObj.log = 'Invalid base detected. Please ensure you are using bases 2 to 32!'
 
     returnObj.err_code = ERROR
     returnObj.var = 0
     return returnObj
   }
 
+  // Check the inVar, and return if error is returned
   var result = errCatch.inVarErrCatch(inVar, inBaseInternal)
 
   if (result.err_code) {
     returnObj.var = result.var
     returnObj.err_code = result.err_code
   } else {
-    console.log('<in> must be a valid round integer, of the given [in_base]! The default [in_base] is decimal.')
+    returnObj.log = '<in> must be a valid round integer, of the given [in_base]! The default [in_base] is decimal.'
     returnObj.err_code = ERROR
   }
 
+  // Set our output var to the required base
   returnObj.var = returnObj.var.toString(outBaseInternal)
 
+  // return the final object
   return returnObj
 }
 
 if (require.main === module) {
   cli.parseAndExit().then(argv => {
     var convObj = numConv(argv.in, argv.in_base, argv.out_base)
-    console.log('Output: ' + convObj.var.toUpperCase())
+
+    // Only print out the result if it was a success
+    if (convObj.err_code === SUCCESS) {
+      console.log('Output: ' + convObj.var.toUpperCase())
+    } else {
+      console.log(convObj.log)
+    }
   })
 }
 
