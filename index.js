70e2d3ea07c27d62a9f07a8b1592879c05834355
Alex Ford <alxtford@gmail.com>
2019-01-13 00:55:15 +0000

    numConv now returns an object, with the cli program being smarter how it is used

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const ERROR = 0
+const SUCCESS = 1
+
 // cli imported from cli.js
 var cli = require('./cli.js')
 
@@ -8,29 +11,41 @@ var errCatch = require('./errCatch.js')
 function numConv (inVar, inBase, outBase) {
   var inBaseInternal, outBaseInternal
 
+  var returnObj = {
+    var: 0,
+    err_code: SUCCESS
+  }
+
   inBaseInternal = errCatch.baseErrCatch(inBase, true)
   outBaseInternal = errCatch.baseErrCatch(outBase, false)
   if (!inBaseInternal || !outBaseInternal) {
     console.log('Invalid base detected. Please ensure you are using bases 2 to 32!')
-    return
+
+    returnObj.err_code = ERROR
+    returnObj.var = 0
+    return returnObj
   }
 
   var result = errCatch.inVarErrCatch(inVar, inBaseInternal)
 
   if (result.err_code) {
-    inVar = result.var
+    returnObj.var = result.var
+    returnObj.err_code = result.err_code
   } else {
     console.log('<in> must be a valid round integer, of the given [in_base]! The default [in_base] is decimal.')
-    return
+    returnObj.err_code = ERROR
   }
 
-  var output = inVar.toString(outBaseInternal)
-  console.log('Output: ' + output.toUpperCase())
+  returnObj.var = returnObj.var.toString(outBaseInternal)
+
+  return returnObj
 }
 
 if (require.main === module) {
   cli.parseAndExit().then(argv => {
-    numConv(argv.in, argv.in_base, argv.out_base)
+
+    var convObj = numConv(argv.in, argv.in_base, argv.out_base)
+    console.log('Output: ' + convObj.var.toUpperCase())
   })
 }
 
