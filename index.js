f0154eab13a8d73ca9bf704e35d4da7ce82bd6b1
Alex Ford <alxtford@gmail.com>
2019-01-13 00:07:20 +0000

    numCOnv is now exported. numConv can be used as a package!

--- index.js
+++ index.js
@@ -7,32 +7,36 @@ var errCatch = require('./errCatch.js')
 
 var inVar, inBase, outBase
 
+function numConv(inVar, in_base, out_base) {
+  inBase = errCatch.baseErrCatch(in_base, true)
+  outBase = errCatch.baseErrCatch(out_base, false)
+  if (!inBase || !outBase) {
+    console.log('Invalid base detected. Please ensure you are using bases 2 to 32!')
+    return
+  }
+
+  var result = errCatch.inVarErrCatch(inVar, inBase)
+
+  if (result.err_code) {
+    inVar = result.var
+  } else {
+    console.log('<in> must be a valid round integer, of the given [in_base]! The default [in_base] is decimal.')
+    return
+  }
+
+  var output = inVar.toString(outBase)
+  console.log('Output: ' + output.toUpperCase())
+}
+
 if (require.main === module) {
   cli.parseAndExit().then(argv => {
-    inBase = errCatch.baseErrCatch(argv.in_base, true)
-    outBase = errCatch.baseErrCatch(argv.out_base, false)
-    if (!inBase || !outBase) {
-      console.log('Invalid base detected. Please ensure you are using bases 2 to 32!')
-      return
-    }
 
-    var result = errCatch.inVarErrCatch(argv.in, inBase)
+  numConv(argv.in, argv.in_base, argv.out_base)
 
-    if (result.err_code) {
-      inVar = result.var
-    } else {
-      console.log('<in> must be a valid round integer, of the given [in_base]! The default [in_base] is decimal.')
-      return
-    }
-
-    var output = inVar.toString(outBase)
-    console.log('Output: ' + output.toUpperCase())
-
-    // if (!Number.isInteger(inVar) && inBase !== 16) {
-    //   console.log('<in> must be a round integer!')
-    // }
   })
 }
+
+module.exports = numConv
 
 // if (dec < 0) {
 //   // Change negative num to positive. The symbol is appended via the string!
